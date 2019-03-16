947020c635b4e0632de33e9bd2a3777b6d591e05
Romain Jouin <romain.jouin@gmail.com>
2019-03-16 15:14:41 +0100

    chore : cleaning files

--- docker/start_container_spark.sh
+++ docker/start_container_spark.sh
@@ -1,3 +1,3 @@
 docker stop livre_spark
 docker rm livre_spark
-(cd .. && docker run -v $PWD/work:/home/jovyan/work -e GRANT_SUDO=yes --user root  --name livre_spark -p 10000:8888 livre_spark start-notebook.sh --NotebookApp.password='sha1:a8ee1cab76bf:528677ad0222d957c0695c86cf03f03531b87471' )
+(cd .. && docker run -v $PWD/data:/home/jovyan/data  -v $PWD/work:/home/jovyan/work -e GRANT_SUDO=yes --user root  --name livre_spark -p 10000:8888 livre_spark start-notebook.sh --NotebookApp.password='sha1:a8ee1cab76bf:528677ad0222d957c0695c86cf03f03531b87471' )
